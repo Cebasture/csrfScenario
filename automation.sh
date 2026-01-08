558017f9f6a69b23b09be9142a54ca0190fae74d
Cebasture <manav.rohatgi@gmail.com>
2026-01-08 06:14:53 +0000

    bash script for automated mailhog container startup and running the autoLogin.py with virtual env python3 on boot

---
+++ automation.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+set -e
+
+docker start mailhog || true
+#chmod -R 755 /var/lib/docker/volumes/mailhog_data
+
+#source /home/admin/selenium_venv/bin/activate
+
+/home/admin/selenium_venv/bin/python3 /home/admin/autoLogin.py
+
